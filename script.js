a1f114b19aac8c8b2e79a2a31c863ec8d6632bad
SaidmahmudMars <abdusamatovsaidmahmud@gmail.com>
2025-05-17 12:06:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const arr = [
+    `shaftoli`, `olma`, `yusuf`, `jasur`
+]
+let  azzi = confirm(`arraydan narsa olib tashlamoqchimisiz ${arr.length}`)
+
+if (azzi) {
+    arr.pop()
+} else {
+    
+}
+
+let  azz = confirm(`arraydan qolgan malumot ${arr.length}`)
+
+
+console.log(arr);
